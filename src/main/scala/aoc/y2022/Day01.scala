7006ff57c71e8af6761382730bc584614d796616
Jonathan Ostrander <jonathanost@gmail.com>
2022-12-01 08:56:17 -0500

    feat: 2022 day 1

--- src/main/scala/aoc/y2022/Day01.scala
+++ src/main/scala/aoc/y2022/Day01.scala
@@ -2,8 +2,16 @@ package aoc
 package y2022
 
 object Day01 extends Day(1, 2022) {
-  override def partOne(): String = "???"
+  val elves = input.foldLeft[List[List[Int]]](Nil) {
+    case (Nil, "") => Nil
+    case (Nil, next) => List(next.toInt) :: Nil
+    case (list, "") => Nil :: list
+    case (head :: tail, next) => (next.toInt :: head) :: tail
+  }
+
+  val totals = elves.map(_.sum).sorted.reverse
+
+  override def partOne(): String = totals.head.toString
   
-  override def partTwo(): String = "???"
-  
+  override def partTwo(): String = totals.take(3).sum.toString
 }
